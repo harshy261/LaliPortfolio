212b048acab5163cecf3c54d20f45587fe0f8169
harshy261 <iyadavharsh261@gmail.com>
2024-11-10 18:27:38 +0530

    first commit

---
+++ src/data/projectsData.js
@@ -0,0 +1,151 @@
+import one from '../assets/svg/projects/one.svg'
+import two from '../assets/svg/projects/two.svg'
+import three from '../assets/svg/projects/three.svg'
+import four from '../assets/svg/projects/four.svg'
+import five from '../assets/svg/projects/five.svg'
+import six from '../assets/svg/projects/six.svg'
+import seven from '../assets/svg/projects/seven.svg'
+import eight from '../assets/svg/projects/eight.svg'
+import nine from '../assets/svg/projects/nine.svg'
+import ten from '../assets/svg/projects/ten.svg'
+import eleven from '../assets/svg/projects/eleven.svg'
+import twelve from '../assets/svg/projects/twelve.svg'
+import thirteen from '../assets/svg/projects/thirteen.svg'
+
+
+export const projectsData = [
+    {
+        id: 1,
+        projectName: 'Study Notion',
+        projectDesc: 'This project aims to build an student learning platform where user can access different courses.',
+        tags: ['React', 'Tailwind', 'NodeJs', 'MongoDB'],
+        code: 'https://github.com/Lalita14ritgithub/study-notion',
+        demo: 'https://study-notion-website-seven.vercel.app/',
+        image: one
+    },
+    {
+        id: 2,
+        projectName: 'Recipe Sharing Website',
+        projectDesc: 'This website built using the MERN stack, allowing users to browse, share, and discover diverse recipes from various cuisines.',
+        tags: ['React', 'Tailwind', 'Node.Js', 'MongoDB'],
+        code: 'https://github.com/Lalita14ritgithub/Recipe-sharing-website',
+        demo: 'https://github.com/Lalita14ritgithub/Recipe-sharing-website',
+        image: two
+    },
+    {
+        id: 3,
+        projectName: 'Chat App',
+        projectDesc: 'This project built with the MERN stack, MongoDB, JWT, and Socket.IO, enabling users to securely chat with each other through an interactive interface.',
+        tags: ['JavaScript', 'React', 'Node.Js', 'MongoDB'],
+        code: 'https://github.com/Lalita14ritgithub/chat-application',
+        demo: 'https://github.com/Lalita14ritgithub/chat-application',
+        image: three
+    },
+    {
+        id: 4,
+        projectName: 'Razorpay Clone',
+        projectDesc: 'Developed an intuitive UI for seamless user experience using Html, Tailwind CSS, and NodeJs.',
+        tags: ['HTML', 'Tailwind CSS', 'NodeJs'],
+        code: 'https://github.com/Lalita14ritgithub/RazorpayClone',
+        demo: 'https://razorpay-clone-mauve.vercel.app/',
+        image: four
+    },
+    {
+        id: 5,
+        projectName: 'Discord Clone',
+        projectDesc: 'Create an intuitive UI for seamless user experience using Html, Tailwind CSS, and NodeJs.',
+        tags: ['HTML', 'Tailwind CSS'],
+        code: 'https://github.com/Lalita14ritgithub/DiscordClone',
+        demo: 'https://discord-clone-site.netlify.app/',
+        image: five
+    },
+    {
+        id: 6,
+        projectName: 'StopWatch',
+        projectDesc: 'A functional stopwatch built with HTML, CSS, and JavaScript, featuring start, stop, and reset options for tracking time accurately.',
+        tags: ['HTML', 'CSS', 'JavaScript'],
+        code: 'https://github.com/Lalita14ritgithub/stopWatch',
+        demo: 'https://github.com/Lalita14ritgithub/stopWatch',
+        image: six
+    },
+    {
+        id: 7,
+        projectName: 'To-Do List',
+        projectDesc: 'Developed a user friendly to-do list using HTML, CSS, and JavaScript demonstrating strong front-end intuitive interface design. ',
+        tags: ['HTML', 'CSS'],
+        code: 'https://github.com/Lalita14ritgithub/To-Do-List',
+        demo: 'https://my-todo-pro.netlify.app/',
+        image: seven
+    },
+    {
+        id: 8,
+        projectName: 'DadJoke async await',
+        projectDesc: 'A Dad Joke generator built with HTML, CSS, and JavaScript using async/await to fetch and display random jokes seamlessly.',
+        tags: ['Html', 'CSS', 'JavaScript'],
+        code: 'https://github.com/Lalita14ritgithub/DadJoke-async-await',
+        demo: 'https://dadjoke-async-await.netlify.app/',
+        image: eight
+    },
+    {
+        id: 9,
+        projectName: 'BMI Calculator',
+        projectDesc: 'Created a user friendly BMI calculator offers real-time, accurate results with input validation.',
+        tags: ['Javascript', 'CSS'],
+        code: 'https://github.com/Lalita14ritgithub/BMI-Calculator',
+        demo: 'https://bmi-calculator-projrect.netlify.app/',
+        image: nine
+    },
+    {
+        id: 10,
+        projectName: 'Weather App',
+        projectDesc: 'This Project inform the user about the weather conditions of requested location using Weather API.',
+        tags: ['Html', 'CSS', 'JavaScript'],
+        code: 'https://github.com/Lalita14ritgithub/weather-application',
+        demo: 'https://weatherapplication-pro.netlify.app/',
+        image: ten
+    },
+    {
+        id: 11,
+        projectName: 'Image Gallery',
+        projectDesc: 'This is a front-end web development project using Html, CSS and Animations demonstrating strong front-end skills.',
+        tags: ['Html', 'CSS'],
+        code: 'https://github.com/Lalita14ritgithub/Image-Gallery',
+        demo: 'https://imagegallery-project.netlify.app/',
+        image: eleven
+    },
+    {
+        id: 12,
+        projectName: 'Sliding Login Registration',
+        projectDesc: 'Developed a sleek sliding login and signup page usinf HTML, CSS and JavaScript demonstarting strong front-end skills.',
+        tags: ['Html', 'CSS'],
+        code: 'https://github.com/Lalita14ritgithub/Sliding-Login-Registraion',
+        demo: 'https://sliding-login-registration.netlify.app/',
+        image: twelve
+    },
+    {
+        id: 13,
+        projectName: 'Parallax Website',
+        projectDesc: 'Crafted a dynamic parallax website using HTML, CSS fusing scrolling effecs for an engaging user experience.',
+        tags: ['Html', 'CSS'],
+        code: 'https://github.com/Lalita14ritgithub/parallax-website',
+        demo: 'https://para-web-site.netlify.app/',
+        image: thirteen
+    },
+    
+]
+
+
+// Do not remove any fields.
+// Leave it blank instead as shown below
+
+/* 
+{
+    id: 1,
+    projectName: 'Car Pooling System',
+    projectDesc: '',
+    tags: ['Flutter', 'React'],
+    code: '',
+    demo: '',
+    image: ''
+}, 
+*/
